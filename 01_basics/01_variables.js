3e0455d12173e0b2e5b9232f2c71ff908dc0d047
Himanshi Agrawal <131573516+agrawal-himanshi@users.noreply.github.com>
2023-10-01 18:39:20 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 15545
+let accountEmail = "himanshiagrawal@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+//no prefer to use 
+let accounState;
+
+// accountId = 767564
+console.log(accountId)
+
+// prefer not to use var bcz of issue in block scope and functional scope.
+
+accountEmail = "hussrghh@gmail.com"
+accountPassword = "4545"
+accountCity = "banglore"
+console.table([accountId, accountEmail, accountPassword, accountCity, accounState])
