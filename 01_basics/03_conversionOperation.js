654d70077a53601e59e9ea7337a284b0a8e69eb6
Himanshi Agrawal <131573516+agrawal-himanshi@users.noreply.github.com>
2023-10-11 15:17:12 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let Score = null
+// let Score1 = undefined
+// let Score2 = false
+
+// console.log(typeof Score)
+// console.log(typeof (Score))
+
+// let Value = Number(Score)
+// console.log(typeof Value)
+// console.log(Value)
+
+// "33" => 33
+// "33abc" => NaN
+//null => 0
+//undefined => Nan
+//true => 1
+//false => 0
+
+// let LogIn = ""
+// let BooleanLogIn = Boolean(LogIn)
+// console.log(BooleanLogIn)
+// console.log(typeof BooleanLogIn)
+
+// 1 => true ; 0 => false
+// "" => false(empty string)
+// "abc" => true 
+
+let no = 0 
+let stringNo = String(no)
+console.log(stringNo) 
+console.log(typeof(stringNo)) 
+
+// anything can be covert to string 
