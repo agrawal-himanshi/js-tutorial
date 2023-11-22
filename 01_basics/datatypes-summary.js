56da14812d6ff9e3f8737c076485a5c8fefe90dc
Himanshi Agrawal <131573516+agrawal-himanshi@users.noreply.github.com>
2023-11-22 05:06:46 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// primitive(call by value0)
+
+//approx 7
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol 
+// BigInt
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const temp = null
+let userName;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log( id === anotherId);
+
+
+
+
+
+// non primitive(reference type)
+// approx 3-4 
+// arrays
+// objects
+// functions
+
+const heroes = ["shaktiman" , "nagraj"];
+
+let myObj = {
+    name:"himanshi",
+    age: 21,
+}
+
+const myFunstion = function(){
+    console.log("hello world");
+}
+
+console.log(typeof temp)
+// js is dynamically typed language
+
+
