3924437caee5544b69d4c69a6c000543a93a73c8
Himanshi Agrawal <131573516+agrawal-himanshi@users.noreply.github.com>
2023-11-23 06:20:05 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+const myArray = [0,1,2,3,4]
+const myArr2 = new Array(2,4,6,8)
+// console.log(myArr2)
+// console.log(myArray[0])
+
+//array methods
+
+// myArray.push(6)
+// myArray.push(8)
+// myArray.pop()
+
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(4))
+// console.log(myArray.indexOf(4))
+
+// console.log(myArray)
+
+
+// const newArr = myArray.join()
+// console.log(myArray)
+// console.log(newArr)
+// console.log(typeof(newArr))
+
+
+// slice or splice
+
+console.log("A ", myArray)
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1)
+
+console.log("B ", myArray)
+
+const myn2 = myArray.splice(1,3)
+console.log("C ", myArray)
+console.log(myn2)
+
